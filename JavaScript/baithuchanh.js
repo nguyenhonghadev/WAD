bae2f23d5064b1fe061838f1440506e1516ade53
Nguyễn Hồng Hà <142300051+nguyenhonghadev@users.noreply.github.com>
2023-10-04 13:40:25 +0700

    Hàm

--- JavaScript/baithuchanh.js
+++ JavaScript/baithuchanh.js
@@ -3,7 +3,11 @@
  <head>
     <tile>1st JavaScript</tile>
     <script>
+        function hello() {
         alert(welcome to JS by Nguyen Hong Ha);
+            
+        }
+        
 
     </script>
  </head> 
