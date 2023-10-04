f2a97c1a5a9a6d259461fcf0a08e3a9e7a6dd254
Nguyễn Hồng Hà <142300051+nguyenhonghadev@users.noreply.github.com>
2023-10-04 13:44:50 +0700

    Hàm và Sự Kiện

--- JavaScript/baithuchanh.js
+++ JavaScript/baithuchanh.js
@@ -3,6 +3,7 @@
  <head>
     <tile>1st JavaScript</tile>
     <script>
+        //hàm hiển thị alert
         function hello() {
         alert(welcome to JS by Nguyen Hong Ha);
             
@@ -10,7 +11,10 @@
         
 
     </script>
+    <tile>1st JavaScript</tile>
  </head> 
  <body>
+    <h1>1st JavaScript</h1>
+    <button onclick="hello()">Click Here !</button>
 </body>  
 </html>
