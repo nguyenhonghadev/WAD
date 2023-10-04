bf17d21062612e9642ee5c500a97879d270917b8
Nguyễn Hồng Hà <142300051+nguyenhonghadev@users.noreply.github.com>
2023-10-04 13:30:13 +0700

    JS Firrt

---
+++ JavaScript/baithuchanh.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+ <head>
+    <tile>1st JavaScript</tile>
+    <script>
+        alert(welcome to JS by Nguyen Hong Ha);
+
+    </script>
+ </head> 
+ <body>
+</body>  
+</html>
