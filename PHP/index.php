4d2e71b07c2c8a658d58289ab3d98e455a57420f
Nguyễn Hồng Hà <142300051+nguyenhonghadev@users.noreply.github.com>
2023-11-07 16:17:36 +0700

    silde_21

--- PHP/index.php
+++ PHP/index.php
@@ -1,8 +1,18 @@
-<?php
-echo "MY FIRT PHP FILE";
-echo "<h3> Hello Word ! </h3>";
-echo "select";
-echo "<option>1</option>";
-echo "<option>2</option>";
-echo "</select>";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nguyen Hong Ha</title>
+</head>
+<body>
+   <?php 
+   echo "MY FIRT HTML";
+   ?>
+<h3>  Hello Word </h3>
+<select>
+    <option  valua="1">1</option>
+    <option  valua="2">2</option>
+</select>
+</body>
+</html>
