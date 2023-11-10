690a953df03db301352b42199ce6b159dcd70b1c
Nguyễn Hồng Hà <142300051+nguyenhonghadev@users.noreply.github.com>
2023-11-10 10:54:21 +0700

    FILE 5-PHPFORM

---
+++ PHP/FILE 5-FORMGET.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nguyen Hong Ha</title>
+</head>
+
+<body>
+Welcome <?php echo $_GET["name"]; ?> <br>
+nhập mail của bạn: <?php echo $_GET["email"]; ?> 
+
+</body>
+</html>
