99262c0e30ab4503a64effdbec5c975b4293e3d9
Nguyễn Hồng Hà <142300051+nguyenhonghadev@users.noreply.github.com>
2023-11-10 10:13:37 +0700

    BIẾN (VARIABLE) TRONG PHP

--- PHP/index.php
+++ PHP/index.php
@@ -1,17 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nguyen Hong Ha</title>
 </head>
+
 <body>
-   <?php 
-  $longAccount = 10.000.000;
-  echo "số tien trong tai khoan Long : $longAccount";
-  echo "HUMG chuyen tien : $HUMGPAy";
-  $longAccount = $longAccount + $HUMGPAy;
-  echo "số tiền trong tài khoản long : $longAccount";
-  ?>
+    <?php 
+ $subject = "Lập trình ứng dụng Web";
+ $universityName = HUMg;
+ $firstNumber = 2 ;
+ $seconndtNumber = 2;
+ echo "Học". $subject. "Ỏ phòng 502 tại trường". $universityName . "<br>" ;
+ echo "Học $subject ở phòng 502 tại trường $universityName <br>"
+ echo "<h1> $univesityName </h1>" ;
+ echo $firstNumber + $seconndtNumber ;
+ 
 </body>
 </html>
