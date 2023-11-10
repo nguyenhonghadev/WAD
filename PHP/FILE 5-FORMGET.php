626bdfb159176cdd6bc9320272fbcb7cdf4cb593
Nguyễn Hồng Hà <142300051+nguyenhonghadev@users.noreply.github.com>
2023-11-10 10:56:22 +0700

    TẠO FILE 5-FORMGET.PHP

--- PHP/FILE 5-FORMGET.php
+++ PHP/FILE 5-FORMGET.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nguyen Hong Ha</title>
+    <title>Nguyen Hong Ha </title>
 </head>
 
 <body>
