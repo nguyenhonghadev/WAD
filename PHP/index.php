4dd35e1133b476c149aa5f9c1b0b4026e97b5495
Nguyễn Hồng Hà <142300051+nguyenhonghadev@users.noreply.github.com>
2023-11-12 20:09:44 +0700

    baithuchanh GET

--- PHP/index.php
+++ PHP/index.php
@@ -1,52 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kiểm tra tứ giác là hình gì</title>
-</head>
-<body>
-    <form action="index.php" method="GET">
-        <div>
-        Nhập Số a:<br>
-        <input type="number" name="a"><br>
-        Nhập Số b:<br>
-        <input type="number" name="b"><br>
-        Nhập Số c:<br>
-        <input type="number" name="c"><br>
-        <input type="submit" value="Gửi">
-    </form>
+<?php
 
-        </div>
-    </form>
-    <?php
-if( isset($_GET['a']) && isset($_GET['b']) && isset($_GET['c']) ){ 
-    $a = (float) $_GET["a"];
-   $c = (float) $_GET["b"];
-   $b = (float) $_GET["c"];
-if ($a == 0){
-    echo "Phương trình không phải là phương trình bậc 2.";
-} else {
-    $delta = pow($b, 2) - 4 * $a * $c;
-    if ($a == 0 && $delta == 0){
-        echo "Phương trình có một nghiệm kép là: x = 0";
-    } elseif ($a == 0){
-        echo "Phương trình vô nghiệm";
-    } else {
-        if ($delta > 0) {
-            $x1 = (-$b + sqrt($delta)) / (2 * $a);
-            $x2 = (-$b - sqrt($delta)) / (2 * $a);
-            echo "Phương trình có hai nghiệm phân biệt là: x1 = $x1 và x2 = $x2";
-        } elseif ($delta == 0) {
-            $x = -$b / (2 * $a);
-            echo "Phương trình có một nghiệm kép là: x = $x";
-        } else {
-            echo "Phương trình vô nghiệm";
-        }
-    }
-}
+// Kiểm tra xem phương thức yêu cầu là GET hay không
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+
+    // Lấy các thông tin đăng ký form "form.html"
+    $hoTen = $_GET["hoTen"];
+    $PW = $_GET["PW"];
+    $email = $_GET["email"];
+    $Sdt = $_GET["Sdt"];
+
+    //hiển thị thông tin thông tin đăng ký
+    echo "Tên người dùng: " . $hoTen;
+    echo "<br>";
+    echo "Mật khẩu: " . $PW;
+    echo "<br>";
+    echo "Email: " . $email;
+    echo "<br>";
+    echo "Số điện thoại: " . $Sdt;
 }
 
 ?>
-</body>
-</html>
