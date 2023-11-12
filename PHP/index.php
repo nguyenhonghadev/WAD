b25dbd87a4f2845485d8bdaf0e6f32cdd1eea6f1
Nguyễn Hồng Hà <142300051+nguyenhonghadev@users.noreply.github.com>
2023-11-12 17:05:21 +0700

    slide 30_04

--- PHP/index.php
+++ PHP/index.php
@@ -1,26 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nguyễn Hồng Hà</title>
+    <title>Kiểm tra tứ giác là hình gì</title>
 </head>
-<html>
-
-<head>
-    <title>Tìm chữ h</title>
-</head>
-
 <body>
-<?php 
-$name=" Chúng tôi là sinh viên trường đại học mở địa chất";
-$temp = substr_count($name ,"h");
-echo $name;
-echo"<br>Có  " . $temp . " chữ h xuất hiện trong chuỗi trên";
+    <form action="index.php" method="GET">
+        <div>
+        Nhập vào cạnh của tứ giác:<br>
+        <input type="number" name="a"><br>
+        Nhập vào cạnh còn lại của tứ giác:<br>
+        <input type="number" name="b"><br>
+        <input type="submit" value="Gửi">
+    </form>
 
-?>
-</html>
+        </div>
+    </form>
+    <?php
+    if(isset($_GET['a']) && isset($_GET['b']))
+    { 
+    $canh1 =(float) $_GET["a"];
+    $canh2 =(float) $_GET["b"];
+    if ($canh1 ==$canh2) {
+        echo"Tứ giác là hình vuông";
+    }
+    else
+    echo"Tứ giác là hình chữ nhật";
+}
+
+    ?>
 </body>
-
 </html>
