d365bc4ccab155c4c01a9e4b7b4fb81ec10c0ff7
Nguyễn Hồng Hà <142300051+nguyenhonghadev@users.noreply.github.com>
2023-11-12 20:16:35 +0700

    baithuchanh_POST

--- PHP/index.php
+++ PHP/index.php
@@ -1,13 +1,13 @@
 <?php
 
-// Kiểm tra xem phương thức yêu cầu là GET hay không
-if ($_SERVER["REQUEST_METHOD"] == "GET") {
+// Kiểm tra xem phương thức yêu cầu là POST hay không
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Lấy các thông tin đăng ký form "form.html"
-    $hoTen = $_GET["hoTen"];
-    $PW = $_GET["PW"];
-    $email = $_GET["email"];
-    $Sdt = $_GET["Sdt"];
+    $hoTen = $_POST["hoTen"];
+    $PW = $_POST["PW"];
+    $email = $_POST["email"];
+    $Sdt = $_POST["Sdt"];
 
     //hiển thị thông tin thông tin đăng ký
     echo "Tên người dùng: " . $hoTen;
