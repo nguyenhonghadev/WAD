d85a5094f60a71b3a7bf9c94048406a32426d8f8
Nguyễn Hồng Hà <142300051+nguyenhonghadev@users.noreply.github.com>
2023-11-17 00:36:49 +0700

    slide 22

--- PHP/index.php
+++ PHP/index.php
@@ -2,18 +2,21 @@
 <html lang="en" dir="ltr">
     <head>
         <meta charset="utf-8">
-        <title></title>
+        <title> Christmas?</title>
     </head>
     <body>
-        <h2>Diem chuan</h2>
-        <form action="code.php" method="get">
-            <p>Nhap diem toan</p>
-            <input type="number" name="toan"><br>
-            <p>Nhap diem ly</p>
-            <input type="number" name="ly"><br>
-            <p>Nhap diem hoa</p>
-            <input type="number" name="hoa"><br><br>
-            <input type="submit" value="Convert">
-        </form>
+        <h1>Is it Christmas?</h1>
+        <?php
+            $is_christmas = false;
+            if (date('m') == 12 && date('d') == 25) {
+                $is_christmas = true;
+            }
+            if ($is_christmas){
+                echo "Yes";
+            }
+            else{
+                echo "No";
+            }
+        ?>
     </body>
 </html>
