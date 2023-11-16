a22b2865b18efc927f041222ae53c91f7a14501c
Nguyễn Hồng Hà <142300051+nguyenhonghadev@users.noreply.github.com>
2023-11-17 01:11:18 +0700

    slide23

--- PHP/code.php
+++ PHP/code.php
@@ -6,14 +6,12 @@
     </head>
     <body>
         <?php
-   $tongdiem = $_GET["toan"] + $_GET["ly"] + $_GET["hoa"];
-   if($tongdiem<17) echo"diem cua ban khong du dau cac nganh ";
-   else
-   echo "diem cua ban lon hơn diem chuan cac nganh sau :<br> ";
-   if($tongdiem>=15) echo "Moi truong<br> ";
-   if($tongdiem>=17) echo "Dia chat<br> ";
-   if($tongdiem>=18) echo "CNTT<br> KHDL <br>";
-   if($tongdiem>=22) echo "CNTT chat luong cao<br>";
+            $ccao = $_POST["cao"];
+            $cnang = $_POST["nang"];
+            $Gioitinh = $_POST["gioitinh"];
+        if($Gioitinh=="nam" && $ccao>160 && $cnang>50) echo "Dat yeu cau";
+        if($Gioitinh=="nam" && $ccao>150 && $cnang>50 && $cnang<70) echo "Dat yeu cau";
+       else echo "khong dat yc"
 
         ?>
     </body>
--- PHP/index.php
+++ PHP/index.php
@@ -2,21 +2,21 @@
 <html lang="en" dir="ltr">
     <head>
         <meta charset="utf-8">
-        <title> Christmas?</title>
+        <title></title>
     </head>
     <body>
-        <h1>Is it Christmas?</h1>
-        <?php
-            $is_christmas = false;
-            if (date('m') == 12 && date('d') == 25) {
-                $is_christmas = true;
-            }
-            if ($is_christmas){
-                echo "Yes";
-            }
-            else{
-                echo "No";
-            }
-        ?>
+        <h2>Diem chuan</h2>
+        <form action="code.php" method="post">
+            <p> chọn giới tính của bạn<p>
+            <select name="gioitinh">
+                <option name="nam">Nam</option>
+                <option name="nu">Nữ</option>
+            </select><br>
+            <label for="chieucao">Chiều cao: </label>
+            <input type="number" name="cao"><br>
+            <label for="cannang">Cân nặng: </label>
+            <input type="number" name="nang"><br>           
+            <input type="submit" value="xác nhận">
+        </form>
     </body>
 </html>
