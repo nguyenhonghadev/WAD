f71caf117e92d065677769ccc803e62219982652
Nguyễn Hồng Hà <142300051+nguyenhonghadev@users.noreply.github.com>
2023-11-17 10:50:11 +0700

    slide 28,29

--- PHP/code.php
+++ PHP/code.php
@@ -8,10 +8,16 @@
         <?php
         echo"<h3>Mệnh giá tiền</h3>";
        $giatri = array(22300,27300,1700,120);
+       $giatri[]= 600;
+       $giatri[]= 1600;
+       $giatri[0]= 22200;
+       
        echo "USD $giatri[0] <br>";
        echo "EUR $giatri[1] <br>";
        echo "SGD $giatri[2] <br>";
        echo "JPY $giatri[3] <br>";
+       echo "CHN $giatri[4] <br>";
+       echo "AUD $giatri[5] <br>";
       
         ?>
     </body>
