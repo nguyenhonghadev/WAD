40b01f14cb829b7b3bc04403c4867f1def62f1ac
nguyenhonghadev <ha2k3.dev@gmail.com>
2023-11-24 11:04:00 +0700

    slide 3

--- PHP/index.php
+++ PHP/index.php
@@ -3,14 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dãy số</title>
+    <title>Item</title>
 </head>
 <body>
     <?php
-    for ($x =1 ;$x<=10 ;$x++) {
-        echo "Dãy số là $x <br>";
-        echo "*************<br>";  
-    }
-    ?>
+$items = ["cau.png", "dao.png", "gang.png","kiem.png"];
+
+foreach ($items as $item) {
+  echo "<img src='$item' with = 50px height = 50px alt='$item'";
+
+}
+?>
 </body>
 </html>
