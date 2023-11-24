2dc6e21ee5dc1a70c9461f9f3efd561af58cd4ab
nguyenhonghadev <ha2k3.dev@gmail.com>
2023-11-24 14:18:40 +0700

    slide 16

--- PHP/code.php
+++ PHP/code.php
@@ -6,25 +6,35 @@
     </head>
     <body>
     <?php
-    $toan = $_POST['toan'];
-    $ly = $_POST['ly'];
-    $hoa = $_POST['hoa'];
-    $anh = $_POST['anh'];
-    $van = $_POST['van'];
-    $su = $_POST['su'];
-    $TB = ($toan + $ly + $hoa + $anh + $van + $su) / 6;
-    echo "Học lực của bạn là :";
-    if ($toan < 4 || $ly < 4 || $hoa < 4 || $anh < 4 || $van < 4 || $su < 4) {
-        echo "Yếu";
+    $ngay = $_POST['day'];
+    $thang = $_POST['month'];
+    $nam = $_POST['year'];
+    
+    
+    $check = false;
+    
+    if ($thang == 2 && $nam % 4 == 0) { 
+        if ($ngay <= 29) {
+            $check = true;
+        }
+    } else if ($thang == 2) { 
+        if ($ngay <= 28) {
+            $check = true;
+        }
+    } else if ($thang == 4 || $thang == 6 || $thang == 9 || $thang == 11) { 
+        if ($day <= 30) {
+            $check = true;
+        }
+    } 
+    
+    if ($check == true) {
+        echo "Không tồn tại ngày $ngay tháng $thang năm $nam vui lòng nhập lại";
     } else {
-        if ($TB < 5) {
-            echo "Yếu";
-        } elseif ($TB < 6.4) {
-            echo "Trung bình";
-        } elseif ($TB < 7.9) {
-            echo "Khá";
-        } else  echo "Giỏi";
+        echo "Ngày bạn nhập là: Ngày $ngay tháng $thang Năm $nam";
     }
+    
+   
+   
     ?>
     </body>
 </html>
--- PHP/index.php
+++ PHP/index.php
@@ -1,17 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Item</title>
+  <meta charset="UTF-8">
+  <title>Chọn ngày tháng năm</title>
 </head>
 <body>
-    <?php
-$x =1;
-while($x <=10) {
-    echo"Dãy số là $x <br>";
+  <h2>Chọn ngày tháng năm</h2>
+  <form action="code.php" method="post">
+  <?php
+ 
+  // lấy năm hiện tại 
+   $today = getdate();
+   $curYear = $today["year"];
+   //********************************** */
+   $days = [];
+   for ($i = 1; $i <= 31; $i++) {
+     $days[] = $i;
+   }
+
+   $months = ["Tháng 1", "Tháng 2", "Tháng 3", "Tháng 4", "Tháng 5", "Tháng 6", "Tháng 7", "Tháng 8", "Tháng 9", "Tháng 10", "Tháng 11", "Tháng 12"];
+
+   $years = [];
+   for ($i = 1900; $i <= $curYear; $i++) {
+     $years[] = $i;
+   }
+
+   echo "Ngày: ";
+   echo "<select name='day'>";
+   foreach ($days as $day) {
+     echo "<option value='$day'>$day</option>";
+   }
+   echo "</select>";
+
+   echo "Tháng: ";
+   echo "<select name='month'>";
+   foreach ($months as $month) {
+     echo "<option value='$month'>$month</option>";
+   }
+   echo "</select>";
     
-}
-?>
+    echo"Năm ";
+    echo "<select name='year'>";
+    foreach ($years as $year) {
+      echo "<option value='$year'>$year</option>";
+    }
+    echo "</select>";
+
+   
+  ?>
+  <input type='submit' value='Chọn'>
 </body>
 </html>
