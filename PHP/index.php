f54f961ec044e75b813244f357b3149f5cf572d1
nguyenhonghadev <ha2k3.dev@gmail.com>
2023-12-01 00:18:26 +0700

    slide29

--- PHP/index.php
+++ PHP/index.php
@@ -2,37 +2,38 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Currency Exchange</title>
+    <title></title>
 </head>
 <body>
-    <h3>slide26</h3>
     <form action="" method="get">
-        <p>Amount:</p>
-        <input type="number" name="amount" required>
-        <p>Select currency</p>
-        <select name="currency" required>
-            <option value="USD">USD</option>
-            <option value="EUR">EURO</option>
-            <option value="SGD">SINGAPORE DOLLAR</option>
-            <option value="JPY">JAPANESE YEN</option>
-        </select>
-        <input type="submit" value="Convert">
+        <p>First number: </p>
+        <input type="number" name="firstNumber"><br>
+        <p>Second number: </p>
+        <input type="number" name="secondNumber"><br>
+        <p></p>
+        <input type="submit" name="add" value="+">
+        <input type="submit" name="minus" value="-">
+        <input type="submit" name="multiply" value="*">
+        <input type="submit" name="devide" value="/">
     </form>
 
-    <?php
-    $exchangeRate = array("USD"=>22300, "EUR"=>27300, "SGD"=>17000, "JPY"=>120);
-
-    if (isset($_GET['amount'], $_GET['currency'])) {
-        $amount = $_GET['amount'];
-        $currency = $_GET['currency'];
-
-        if (is_numeric($amount) && $amount >= 0 && array_key_exists($currency, $exchangeRate)) {
-            $convertedAmount = $amount * $exchangeRate[$currency];
-            echo "<p>$amount $currency is equal to " . number_format($convertedAmount) . " VND</p>";
-        } else {
-            echo "<p>Please enter a valid amount and select a valid currency.</p>";
-        }
+<?php
+    if (isset($_GET['add'])){
+        $result = $_GET["firstNumbrt"] + $_GET["secondNumber"];
+        echo $result;
     }
-    ?>
+    elseif (isset($_GET['minus'])){
+        $result = $_GET["firstNumbrt"] - $_GET["secondNumber"];
+        echo $result;
+    }
+    elseif (isset($_GET['multiply'])){
+        $result = $_GET["firstNumbrt"] * $_GET["secondNumber"];
+        echo $result;
+    }
+    elseif (isset($_GET['devide'])){
+        $result = $_GET["firstNumbrt"] / $_GET["secondNumber"];
+        echo $result;
+    }
+?>
 </body>
 </html>
