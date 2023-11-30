bd58785db002f64b0d4c87be49f83b7583735cbb
nguyenhonghadev <ha2k3.dev@gmail.com>
2023-12-01 00:15:40 +0700

    slide26

--- PHP/index.php
+++ PHP/index.php
@@ -2,31 +2,37 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>slide 25</title>
+    <title>Currency Exchange</title>
 </head>
 <body>
-    <h1>Calculator</h1>
-    <form action="code.php" method="post">
-        <label for="toan">Toán:</label>
-        <input type="number" name="toan" step="0.1" min="0" max="10" required><br>
-
-        <label for="ly">Lý:</label>
-        <input type="number" name="ly" step="0.1" min="0" max="10" required><br>
-
-        <label for="hoa">Hóa:</label>
-        <input type="number" name="hoa" step="0.1" min="0" max="10" required><br>
-
-        <label for="tienganh">Tiếng Anh:</label>
-        <input type="number" name="anh" step="0.1" min="0" max="10" required><br>
-
-        <label for="van">Văn:</label>
-        <input type="number" name="van" step="0.1" min="0" max="10" required><br>
-
-        <label for="su">Lịch Sử:</label>
-        <input type="number" name="su" step="0.1" min="0" max="10" required><br>
-
-        <button type="submit">Tính TB</button>
+    <h3>slide26</h3>
+    <form action="" method="get">
+        <p>Amount:</p>
+        <input type="number" name="amount" required>
+        <p>Select currency</p>
+        <select name="currency" required>
+            <option value="USD">USD</option>
+            <option value="EUR">EURO</option>
+            <option value="SGD">SINGAPORE DOLLAR</option>
+            <option value="JPY">JAPANESE YEN</option>
+        </select>
+        <input type="submit" value="Convert">
     </form>
+
+    <?php
+    $exchangeRate = array("USD"=>22300, "EUR"=>27300, "SGD"=>17000, "JPY"=>120);
+
+    if (isset($_GET['amount'], $_GET['currency'])) {
+        $amount = $_GET['amount'];
+        $currency = $_GET['currency'];
+
+        if (is_numeric($amount) && $amount >= 0 && array_key_exists($currency, $exchangeRate)) {
+            $convertedAmount = $amount * $exchangeRate[$currency];
+            echo "<p>$amount $currency is equal to " . number_format($convertedAmount) . " VND</p>";
+        } else {
+            echo "<p>Please enter a valid amount and select a valid currency.</p>";
+        }
+    }
+    ?>
 </body>
 </html>
