d15c724310cfb1699a69e88ee93fd6af9f73d4f0
nguyenhonghadev <ha2k3.dev@gmail.com>
2023-12-01 00:12:37 +0700

    slide 25

--- PHP/code.php
+++ PHP/code.php
@@ -1,40 +1,41 @@
-<!DOCTYPE html>
-<html lang="en" dir= "ltr">
-    <head>
-        <meta charset= "utf-8">
-        <title></title>
-    </head>
-    <body>
-    <?php
-    $ngay = $_POST['day'];
-    $thang = $_POST['month'];
-    $nam = $_POST['year'];
-    
-    
-    $check = false;
-    
-    if ($thang == 2 && $nam % 4 == 0) { 
-        if ($ngay <= 29) {
-            $check = true;
-        }
-    } else if ($thang == 2) { 
-        if ($ngay <= 28) {
-            $check = true;
-        }
-    } else if ($thang == 4 || $thang == 6 || $thang == 9 || $thang == 11) { 
-        if ($day <= 30) {
-            $check = true;
-        }
-    } 
-    
-    if ($check == true) {
-        echo "Không tồn tại ngày $ngay tháng $thang năm $nam vui lòng nhập lại";
+<?php
+
+function tinhDiemTrungBinh($toan, $ly, $hoa, $anh, $van, $su) {
+    $diemTrungBinh = ($toan + $ly + $hoa + $anh + $van + $su) / 6;
+    return $diemTrungBinh;
+}
+
+function xepLoai($diemTrungBinh) {
+    if ($diemTrungBinh < 5) {
+        return "Học sinh yếu";
+    } elseif ($diemTrungBinh >= 5 && $diemTrungBinh <= 6.4) {
+        return "Học sinh trung bình";
+    } elseif ($diemTrungBinh >= 6.5 && $diemTrungBinh <= 7.9) {
+        return "Học sinh khá";
     } else {
-        echo "Ngày bạn nhập là: Ngày $ngay tháng $thang Năm $nam";
+        return "Học sinh giỏi";
     }
-    
-   
-   
-    ?>
-    </body>
-</html>
+}
+$toan = isset($_POST['toan']) ? floatval($_POST['toan']) : 0;
+$ly = isset($_POST['ly']) ? floatval($_POST['ly']) : 0;
+$hoa = isset($_POST['hoa']) ? floatval($_POST['hoa']) : 0;
+$anh = isset($_POST['anh']) ? floatval($_POST['anh']) : 0; 
+$van = isset($_POST['van']) ? floatval($_POST['van']) : 0;
+$su = isset($_POST['su']) ? floatval($_POST['su']) : 0;
+
+if ($toan >= 0 && $toan <= 10 &&
+    $ly >= 0 && $ly <= 10 &&
+    $hoa >= 0 && $hoa <= 10 &&
+    $anh >= 0 && $anh <= 10 && 
+    $van >= 0 && $van <= 10 &&
+    $su >= 0 && $su <= 10) {
+
+    $diemTrungBinh = tinhDiemTrungBinh($toan, $ly, $hoa, $anh, $van, $su);
+    $xepLoai = xepLoai($diemTrungBinh);
+
+    echo "Điểm trung bình: $diemTrungBinh<br>";
+    echo "Xếp loại: $xepLoai";
+} else {
+    echo "Dữ liệu không hợp lệ";
+}
+?>
--- PHP/index.php
+++ PHP/index.php
@@ -1,7 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>slide 25</title>
+</head>
+<body>
+    <h1>Calculator</h1>
+    <form action="code.php" method="post">
+        <label for="toan">Toán:</label>
+        <input type="number" name="toan" step="0.1" min="0" max="10" required><br>
 
-<h1>function</h1>
-<?php
-    require('./13-function.php');
-    $item = array('item1.png', 'item2.png', 'item3.png', 'item4.png');
-    display_image_list($item);
-?>
+        <label for="ly">Lý:</label>
+        <input type="number" name="ly" step="0.1" min="0" max="10" required><br>
+
+        <label for="hoa">Hóa:</label>
+        <input type="number" name="hoa" step="0.1" min="0" max="10" required><br>
+
+        <label for="tienganh">Tiếng Anh:</label>
+        <input type="number" name="anh" step="0.1" min="0" max="10" required><br>
+
+        <label for="van">Văn:</label>
+        <input type="number" name="van" step="0.1" min="0" max="10" required><br>
+
+        <label for="su">Lịch Sử:</label>
+        <input type="number" name="su" step="0.1" min="0" max="10" required><br>
+
+        <button type="submit">Tính TB</button>
+    </form>
+</body>
+</html>
