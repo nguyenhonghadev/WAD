ebf7c80f885f31958280ea08951626a2280f8010
nguyenhonghadev <ha2k3.dev@gmail.com>
2023-12-05 22:12:46 +0700

    slide63

--- PHP/index.php
+++ PHP/index.php
@@ -3,23 +3,66 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Flight Information</title>
+
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 20px;
+        }
+
+        table {
+            font-family: Arial, Helvetica, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+            margin-top: 20px; /* Add some space between the table and other content */
+        }
+
+        table th, table td {
+            border: 1px solid #ddd;
+            padding: 8px;
+        }
+
+        table tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+
+        table tr:hover {
+            background-color: #ddd;
+        }
+
+        table th {
+            padding-top: 12px;
+            padding-bottom: 12px;
+            text-align: left;
+            background-color: #04AA6D;
+            color: white;
+        }
+    </style>
 </head>
 <body>
+
     <?php
-    
     require('connect.php');
     mysqli_set_charset($conn, 'UTF-8');
     $sql = "SELECT * FROM flights";
     $result = $conn->query($sql);
-   $row = $result ->fetch_assoc();
-   echo"<br>"; 
-   print_r($row);
-   $row = $result ->fetch_assoc(); 
-   echo "<br>"; 
-   print_r($row);   
-   $conn ->close();
-    ?>
 
-</body>
-</html>
+    if ($result->num_rows > 0) {
+        echo "<table>";
+        echo "<tr><th>ID</th><th>Origin</th><th>Destination</th><th>Duration</th></tr>";
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr>";
+            echo "<td>" . $row["id"] . "</td>";
+            echo "<td>" . $row["origin"] . "</td>";
+            echo "<td>" . $row["destination"] . "</td>";
+            echo "<td>" . $row["duration"] . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "Không có chuyến bay.";
+    }
+
